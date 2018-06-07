b3d121a5bcc04205683c433dfca6d6622f9a32d5
Maninderpal singh <maninderpal195@gmail.com>
2018-06-07 16:20:15 -0400

    maninder

---
+++ C0738181_Midterm_MADT/src/BinaryToDecimal.java
@@ -0,0 +1,17 @@
+
+ 
+public class BinaryToDecimal {
+ public static int convert(String input) {
+        Long binary =  Long.valueOf(input).longValue();
+        int decimalNumber = 0, i = 0;
+        long remainder;
+        while (binary != 0)
+        {
+            remainder = binary % 10;
+            binary /= 10;
+            decimalNumber += remainder * Math.pow(2, i);
+            ++i;
+        }
+        return decimalNumber;
+    }   
+}
---
+++ C0738181_Midterm_MADT/src/mostfrequent.java
@@ -0,0 +1,24 @@
+
+public class mostfrequent {
+ public static String find(String input) {
+        char[] array = input.toCharArray();
+        int maxCount = 1;
+        char maxChar = array[0];
+ 
+          for(int i = 0, j = 0; i < input.length() - 1; i = j){
+               int count = 1;
+           while (++j < input.length() && array[i] == array[j]) {
+                  count++;
+                }
+         
+          if (count >= maxCount) {
+             maxCount = count;
+             maxChar = array[i];
+         }
+         
+        }
+ 
+    return (maxChar + " = " + maxCount);
+    }
+    
+}
