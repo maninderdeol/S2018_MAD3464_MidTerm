d67a143cbabae77ee01e823455ecab4baa804eff
Maninderpal singh <maninderpal195@gmail.com>
2018-06-07 15:51:18 -0400

    maninder

---
+++ C0738181_Midterm_MADT/src/LambtonStringTools.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author macstudent
+ */
+public class LambtonStringTools {
+    
+    static int a;
+    static int c=0;
+    static int res;
+    
+    static void reverse(String s){
+        char ch[]=new char[s.length()];
+        for(a=0;a<s.length();a++){
+            ch[a]=s.charAt(a);
+        }
+               for(a=s.length()-1;a>=0;a--){
+                   System.out.print(ch[a]);
+               }
+              return s;
+    }
+     
+    public int binaryToDecimal(String s){
+       
+          
+                     
+    }
+    
+    
+    
+}
--- C0738181_Midterm_MADT/src/Main.java
+++ C0738181_Midterm_MADT/src/Main.java
@@ -14,7 +14,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       LambtonStringTools lst = new LambtonStringTools();
+    lst.reverse("Lambton"); 
     }
     
 }
